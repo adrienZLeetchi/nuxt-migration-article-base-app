5a0ae97324814b0f25d23ef9cce46f27afb6516e
adrienZLeetchi <94057361+adrienZLeetchi@users.noreply.github.com>
2022-02-16 15:15:03 +0100

    prettier

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+<script lang="ts">
+import { defineComponent } from "vue";
 import Header from "./components/Header.vue";
+
+export default defineComponent({
+  components: { Header },
+});
 </script>
 
 <template>
